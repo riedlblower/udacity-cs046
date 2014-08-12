e970296c495cd2a30b1eaac5c38d341316e8c29a
riedlblower <github@michaelsexton.com>
2014-08-12 22:43:59 +0100

    Create 20-ReplacePrinter.java

---
+++ PS01/20-ReplacePrinter.java
@@ -0,0 +1,15 @@
+//
+// complete the code to replace the first occurence of 'a' with an 'x'
+//
+
+public class ReplacePrinter
+{
+    public static void main(String[] args)
+    {
+        String course = "Udacity"; //do not modify this line in any way
+        System.out.println(course.replaceFirst("a","x"));
+        //TODO complete the code to replace the first occurence of 'a' with an 'x'
+        //and don't forget to print it out!
+
+    }
+}
