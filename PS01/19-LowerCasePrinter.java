b4ed9c5d843522d5398276c08d29d49fd63810c9
riedlblower <github@michaelsexton.com>
2014-08-12 22:42:45 +0100

    Create 19-LowerCasePrinter.java

---
+++ PS01/19-LowerCasePrinter.java
@@ -0,0 +1,10 @@
+public class LowerCasePrinter
+{
+    public static void main(String[] args)
+    {
+        String course = "Udacity"; //do not modify this line in any way
+        System.out.println(course.toLowerCase());
+        //complete the code to print the string in all lowercase
+
+    }
+}
