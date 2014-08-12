145706500ebfc9f9353eb78244df4d6a868856c0
riedlblower <github@michaelsexton.com>
2014-08-12 22:46:02 +0100

    Create Q2-NameInBox.java

---
+++ PS01/Q2-NameInBox.java
@@ -0,0 +1,27 @@
+// Write a program that will print the word Udacity in a box
+// so that it looks exactly like the figure below. Name the program NameInBox
+
+// +-------+
+// |Udacity|
+// +-------+ 
+//
+
+
+//HINT:
+// Use System.out.println(...) to print:
+// +
+// |
+// +
+//
+        
+
+public class NameInBox
+{
+	public static void main(String[] args)
+	{
+        //TODO finish the draft so that your code prints Udacity in a box
+        System.out.println("+-------+");
+        System.out.println("|Udacity|");
+        System.out.println("+-------+");
+	}
+}
