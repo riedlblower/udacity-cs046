7755aed907497379eea5aba56d87fb60c8cadf7e
riedlblower <github@michaelsexton.com>
2014-08-21 22:47:11 +0100

    Create 42-person.java
    Trying Out the this Reference

---
+++ lesson-03-Classes/42-person.java
@@ -0,0 +1,46 @@
+// BlueJ project: lesson3/friends5
+/**
+* 
+*/
+public class Person
+{
+    private String name;
+    private String friends;
+    private int x;
+    private int y;
+    
+    // TODO: Update the person constructor to use the this reference
+    //
+    // It looks a bit cleaner this way stylistically
+    public Person (String name, String pictureName, int x, int y)
+    {
+        this.name = name;
+        friends = "";
+        this.x = x;
+        this.y = y;
+        Picture picture = new Picture(pictureName);
+        picture.translate(x, y);
+        picture.draw();
+
+     
+    }
+    
+    public void addFriend(Person friend) 
+    {
+        friends = friends + friend.name + " ";
+        SmallCircle circle = new SmallCircle(x,y);
+        circle.fill();
+        Line line = new Line(x, y, friend.x, friend.y);
+        line.draw();
+    }
+    
+    public void unfriend(Person nonFriend)
+    {
+        friends = friends.replace(nonFriend.name + " ", "");
+    }
+    
+    public String getFriends() 
+    {
+        return friends;
+    }
+}
