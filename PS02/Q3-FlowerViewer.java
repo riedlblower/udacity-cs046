c315941655adf33437084012790d43c1361d2eb4
riedlblower <github@michaelsexton.com>
2014-08-31 22:54:40 +0100

    Create Q3-FlowerViewer.java
    used for test purposes - not modified

---
+++ PS02/Q3-FlowerViewer.java
@@ -0,0 +1,13 @@
+// you don't need to change or modify this file
+
+public class FlowerViewer
+{
+   public static void main(String[] args)
+   {
+       Flower rose = new Flower(30, 30);
+       rose.draw();
+       
+       rose = new Flower(80, 60);
+       rose.draw();
+   }
+}
