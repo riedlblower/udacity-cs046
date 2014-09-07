fff44cc467fa5043ace0139b101e8f56f65cdddb
riedlblower <github@michaelsexton.com>
2014-09-07 22:16:10 +0100

    Create 10-MathInJava.java
    // BlueJ project: lesson4/turningMathToJava
    // Video: Arithmetic Operations 2
    
    // TODO: The amdahlSpeedup, crossRatio and average methods
    // aren't working yet. Use the fact sheet and your new 
    // knowledge of math expressions in java to fix these methods.

---
+++ 04-Fundamental_Data_Types/10-MathInJava.java
@@ -0,0 +1,52 @@
+// BlueJ project: lesson4/turningMathToJava
+// Video: Arithmetic Operations 2
+
+// TODO: The amdahlSpeedup, crossRatio and average methods
+// aren't working yet. Use the fact sheet and your new 
+// knowledge of math expressions in java to fix these methods. 
+
+public class MathInJava
+{
+    /*
+       Computes the fraction
+
+           1
+      -----------
+           1 - s
+       s + -----
+             n
+
+      See http://en.wikipedia.org/wiki/Amdahl%27s_law
+    */
+    public double amdahlSpeedup(double s, double n)
+    {
+        return 1 / ( s + ((1-s)/n));
+    }
+
+    /*
+        Computes the fraction
+
+        (a - c)(b - d)
+        --------------
+        (b - c)(a - d)
+
+        See http://en.wikipedia.org/wiki/Cross-ratio
+    */
+    public double crossRatio(double a, double b, double c, double d)
+    {
+            return ((a-c)*(b-d)) / ((b-c)*(a-d));
+    }
+
+    /**
+        Computes the average of four values.
+        @param a the first value
+        @param b the second value
+        @param c the third value
+        @param d the fourth value
+        @return the average of the four values
+    */
+    public double average(int a, int b, int c, int d)
+    {
+            return (a+b+c+d)/4.0;
+    }
+}
