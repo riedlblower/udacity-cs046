40b97b36b7b9434e87c3d94668a90ad227c205a1
riedlblower <github@michaelsexton.com>
2014-10-15 22:25:34 +0100

    Create 10-WordTester.java

---
+++ L052-Decisions/10-WordTester.java
@@ -0,0 +1,19 @@
+    public class WordTester
+{
+    public static void main(String[] args)
+    {
+        Word udacity = new Word("Udacity");
+        Word hello = new Word("hello");
+
+        System.out.println(udacity.isVowel(0));
+        System.out.println("Expected: true");
+        System.out.println(udacity.isVowel(1));
+        System.out.println("Expected: false");
+        System.out.println(hello.isVowel(2));
+        System.out.println("Expected: false");
+        System.out.println(hello.isVowel(4));
+        System.out.println("Expected: true");
+
+    }
+}
+
