84c76d85bd0530a0a77064b37317b2707db63806
riedlblower <github@michaelsexton.com>
2014-10-26 21:27:35 +0000

    Create CreditCard.java
            /* Pseudocode for isValid:
             * sum = 0
             * count = 0
             * for each digit starting from the right
             *     count ++
             *     if count is odd
             *         sum = sum + digit
             *     else if (digit < 5)
             *         sum = sum + 2 * digit
             *     else
             *         sum = sum + 2 * digit - 9
             * if the last digit of the sum is zero
             *     The card number is valid
             */

---
+++ L061-Loops/CreditCard.java
@@ -0,0 +1,46 @@
+// Bluej project: lesson6/credit_card
+// Video: Credit Card Checksum
+
+// TODO: Implement the isValid method.
+public class CreditCard
+{
+    private long cardNumber;
+
+    public CreditCard(long cardNumber)
+    {
+        this.cardNumber = cardNumber;
+    }
+
+    /**
+     * Calculates whether this CreditCard has a valid number.
+     * @return true if the number is valid, false if it's not.
+     */
+    public boolean isValid()
+    {
+        long n = cardNumber;
+        int sum = 0;
+        int count = 0;
+
+        while (n > 0)
+        {
+            int digit = (int)(n % 10);
+            count++;
+            if (count % 2 == 1){
+                sum = sum + digit;
+            }
+            else if (digit<5){
+                sum = sum + 2 * digit;
+            }
+            else{
+                sum = sum + (2 * digit) - 9;
+            }
+            n = n/10;
+        }
+        if (sum % 10 == 0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
