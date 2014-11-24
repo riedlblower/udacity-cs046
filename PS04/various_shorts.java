2d63eef4561d3eeabd047c5ff8be837d859db985
riedlblower <github@michaelsexton.com>
2014-11-24 22:08:05 +0000

    Update various_shorts.java

--- PS04/various_shorts.java
+++ PS04/various_shorts.java
@@ -112,4 +112,32 @@ public class Sum
 }
 ==========================================
 
-   
+//hw04_20
+/**
+ * Complete the class. Using a Scanner, ask the user to enter a series of integers
+ * and count the number of even numbers
+ * Use a loop. Do not let bad input (a non-integer) terminate your program with an error.
+ * When the user enters any non-integer, print the number of even integers entered and quit.
+ * Hint: remember hasNextInt()
+ * Hint: use the % operator to determine if a number is even
+ */
+import java.util.Scanner;
+public class CountEvenTester
+{
+   public static void main(String[] args)
+   {
+       System.out.print("Enter an integer: ");
+       Scanner in = new Scanner(System.in);
+       int sum = 0;
+       while (in.hasNextInt())
+       {
+           int num = in.nextInt();
+           if (num%2 == 0){
+               sum++; 
+           }
+           System.out.print("Enter an integer: ");
+       }
+       System.out.print(sum);
+   }
+}
+==========================================
