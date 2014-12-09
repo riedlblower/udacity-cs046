57ca53834cd997d61c539a2af6b99ba9e09a4c9e
riedlblower <github@michaelsexton.com>
2014-12-09 22:10:12 +0000

    Create Q1-MonthPrinter.java

---
+++ PS04/Q1-MonthPrinter.java
@@ -0,0 +1,25 @@
+/**
+ * Exercises the methods in the Month class
+ */
+import java.util.Scanner;
+public class MonthPrinter
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter a month number (1 through 12) " );
+        if (in.hasNextInt() == true) {
+            int monthNumber = in.nextInt();
+            if (monthNumber > 12 || monthNumber < 1){
+                System.out.println("Number must be 1 through 12");
+            }
+            else {
+                Month myMonth = new Month(monthNumber);
+                System.out.println("monthNumber");
+            }
+        }
+        else {
+            System.out.println("Enter an integer");
+        }    
+    }
+}
