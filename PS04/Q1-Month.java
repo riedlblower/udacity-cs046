a9be659f48f2f32fff9d0861fe6a8f1892f8bf5e
riedlblower <github@michaelsexton.com>
2014-12-09 22:12:56 +0000

    Create Q1-Month.java
    //Write a class Month.
    
    //A Month has a constructor that takes the month number as a parameter (1 for January, 2 for February and so forth)
    //   public Month(int monthNumber)
    //If monthNumber is not 1 to 12, set it to 1
    
    //And 2 methods
    //   public String getMonthName() Use if / else if /else if ...
    //   public int getNumberOfDays() Do NOT use a separate if / else branch for each month. Use Boolean operators (&&, ||, !).
    //       You will have an if then else if then else.
    //       For February return 28
    //
    //These are implemented for you as stubs
    
    //Now write a class MonthPrinter which will instantiate a Month object, ask the user for a month number
    //and print the name of the month and the number or days.
    
    //If the month is 6, the output should look exactly like this
    //June 30
    
    //If the user enters an invalid month number print this error message: "Number must be 1 through 12"
    
    //If the user enters a non-integer, give this error message: "Not an integer. Terminating"
    
    //To be considered correct, you must enter the message exactly as specified.
    
    // Need help starting this question? In the lesson titled 
    // "Starting points: Problem Set Questions", go to the
    // problem titled "Problem Set 4 - Question 1" for some tips on 
    // how to begin.
    
    //In MonthPrinter, ask the user for a number 1 to 12 and create a month object with that number

---
+++ PS04/Q1-Month.java
@@ -0,0 +1,33 @@
+public class Month
+{
+    //instance variables
+
+    /**
+     * Contructs a Month
+     * @param the month number. Must be 1 to 12. For any other number, the month number is set to 1
+     */
+    public Month(int theMonthNumber)
+    {
+
+    }
+
+    /**
+     * Gets the name of this month
+     * @return the name of this month
+     */
+    public String getMonthName()
+    {
+
+        return null;
+    }
+
+    /**
+     * Gets the number of days in this month
+     * @return the number of days in this month in a non-leap year
+     */
+    public int getNumberOfDays()
+    {
+
+        return 0;
+    }
+}
