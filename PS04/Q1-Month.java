66d6559ea93f979359a87e9465441934b126102c
riedlblower <github@michaelsexton.com>
2014-12-10 23:01:02 +0000

    Update Q1-Month.java

--- PS04/Q1-Month.java
+++ PS04/Q1-Month.java
@@ -1,14 +1,19 @@
 public class Month
 {
     //instance variables
-
+    private int theMonthNumber;
     /**
      * Contructs a Month
      * @param the month number. Must be 1 to 12. For any other number, the month number is set to 1
      */
     public Month(int theMonthNumber)
     {
-
+        if (theMonthNumber < 1 || theMonthNumber > 12){
+            this.theMonthNumber = 1;
+        }
+        else {
+            this.theMonthNumber = theMonthNumber;
+        }
     }
 
     /**
@@ -17,8 +22,44 @@ public class Month
      */
     public String getMonthName()
     {
-
-        return null;
+        String monthName = "";
+        if (theMonthNumber == 1){
+            monthName = "January";
+        }
+        else if (theMonthNumber == 2){
+            monthName = "February";
+        }
+        else if (theMonthNumber == 3){
+            monthName = "March";
+        }
+        else if (theMonthNumber == 4){
+            monthName = "April";
+        }
+        else if (theMonthNumber == 5){
+            monthName = "May";
+        }
+        else if (theMonthNumber == 6){
+            monthName = "June";
+        }
+        else if (theMonthNumber == 7){
+            monthName = "July";
+        }
+        else if (theMonthNumber == 8){
+            monthName = "August";
+        }
+        else if (theMonthNumber == 9){
+            monthName = "September";
+        }
+        else if (theMonthNumber == 10){
+            monthName = "October";
+        }
+        else if (theMonthNumber == 11){
+            monthName = "November";
+        }
+        else if (this.theMonthNumber == 12){
+            monthName = "December";
+        }
+        return monthName;
     }
 
     /**
@@ -27,7 +68,16 @@ public class Month
      */
     public int getNumberOfDays()
     {
-
-        return 0;
+        int days=0;
+        if (theMonthNumber == 4 || theMonthNumber == 9 || theMonthNumber == 11 || theMonthNumber == 6){
+            days = 30;
+        }
+        else if (theMonthNumber == 2){
+            days = 28;
+        }
+        else {
+            days = 31;
+        }
+        return days;
     }
 }
