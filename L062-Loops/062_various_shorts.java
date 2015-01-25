73fd251f8c24c47a2c61ef48b0fb167ad62ca9c0
riedlblower <github@michaelsexton.com>
2015-01-25 21:56:36 +0000

    Update 062_various_shorts.java

--- L062-Loops/062_various_shorts.java
+++ L062-Loops/062_various_shorts.java
@@ -20,3 +20,27 @@ public class InputValidation
         System.out.println("Thank you for entering " + value);
     }
 }
+
+//======================================
+// unit6.2-6
+import java.util.Scanner;
+// Write a program that helps a user compute their average time
+// for sprinting 100 meters.
+public class AverageDemo
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        int count = 0;
+        double sum = 0;
+
+        System.out.print("Enter a value, 0 to quit: ");
+
+        // TODO: Collect values from the user until they enter a 0.
+        // Track the sum of all the inputs so you can calculate the
+        // average at the end
+
+        double average = sum / count;
+        System.out.printf("Average: %.2f\n", average);
+    }
+}
