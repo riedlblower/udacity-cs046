d9c9d408b03cd89bf06323a918ae9a0659bd3ddf
riedlblower <github@michaelsexton.com>
2015-01-25 22:16:08 +0000

    Update 062_various_shorts.java

--- L062-Loops/062_various_shorts.java
+++ L062-Loops/062_various_shorts.java
@@ -33,14 +33,28 @@ public class AverageDemo
         Scanner in = new Scanner(System.in);
         int count = 0;
         double sum = 0;
-
-        System.out.print("Enter a value, 0 to quit: ");
+        double value = 0;
+        boolean done;
 
         // TODO: Collect values from the user until they enter a 0.
         // Track the sum of all the inputs so you can calculate the
         // average at the end
-
+        done = false;
+        while (!done)
+        {
+            System.out.print("Enter a value, 0 to quit: ");
+            value = in.nextDouble();
+            if (value != 0) {
+                    sum += value;
+                    count = count + 1;
+            }
+            else {
+                done = true;
+            }
+        }
+            
         double average = sum / count;
         System.out.printf("Average: %.2f\n", average);
     }
 }
+//======================================
