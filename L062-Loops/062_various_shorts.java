344cc57ed99403f5d0b0c2bf7dcfa7480d5d4e60
riedlblower <github@michaelsexton.com>
2015-01-26 22:12:40 +0000

    Update 062_various_shorts.java

--- L062-Loops/062_various_shorts.java
+++ L062-Loops/062_various_shorts.java
@@ -58,3 +58,34 @@ public class AverageDemo
     }
 }
 //======================================
+// unit6.2-7
+import java.util.Scanner;
+
+// TODO: Update your method average() so that it can accept
+// altitudes (which can be positive, negative or zero).
+// Use Q as a sentinel value instead of 0.
+// Use the scanner method hasNextDouble to control your loop.
+// Remember to change the prompt.
+public class Average
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        double value;
+        int count = 0;
+        double sum = 0;
+
+        System.out.print("Enter a value, Q to quit: ");
+
+        while (in.hasNextDouble())
+        {
+            value = in.nextDouble();
+            sum = sum + value;
+            count++;
+            System.out.print("Enter a value, Q to quit: ");
+        }
+        double average = sum / count;
+        System.out.printf("Average: %.2f\n", average);
+    }
+}
+//======================================
