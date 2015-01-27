57c552e2699b754692e35c6a0211df09be15907b
riedlblower <github@michaelsexton.com>
2015-01-27 22:16:09 +0000

    Update 062_various_shorts.java

--- L062-Loops/062_various_shorts.java
+++ L062-Loops/062_various_shorts.java
@@ -89,3 +89,36 @@ public class Average
     }
 }
 //======================================
+import java.util.Scanner;
+// unit6.2-9
+// Update your program so that the user supplies at least
+// one input.
+// Use Q as a sentinel value.
+// Start prompting for the sentinel value after the first input.
+// If the user doesn't provide even the first input, print Error: No input
+public class Average
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        int count = 0;
+        double sum = 0;
+        System.out.print("Enter a value: ");
+        while (in.hasNextDouble())
+        {
+            double value = in.nextDouble();
+            sum = sum + value;
+            count++;
+            System.out.print("Enter a value, Q to quit: ");
+        }
+        if (count==0)
+        {
+            System.out.printf("Error: No input");
+        }
+        else
+        {
+            double average = sum / count;
+            System.out.printf("Average: %.2f\n", average);
+        }
+    }
+}
