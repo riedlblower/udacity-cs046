ed7b829ec3934b698592d3d3d6c662774a017dda
riedlblower <github@michaelsexton.com>
2015-01-30 21:51:19 +0000

    Create 062_various_shorts_B.java

---
+++ L062-Loops/062_various_shorts_B.java
@@ -0,0 +1,28 @@
+// BlueJ project: lesson6/counting_matches
+
+import java.util.Scanner;
+public class CountingMatches
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        int counter = 0;
+
+        // TODO: Prompt the user for a value until they enter Q
+        // count how many times the user enters a negative number
+        // using the variable counter
+
+        System.out.print("Enter a value, Q to quit: " );
+
+        while (in.hasNextDouble())
+        {
+            double value = in.nextDouble();
+            if (value<0)
+            {
+                counter++;
+            }
+            System.out.print("Enter a value, Q to quit: ");
+        }
+        System.out.println("The water line fell on " + counter + " years.");
+    }
+}
