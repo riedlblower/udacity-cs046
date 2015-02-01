ed1585d20d510a9fd2f5ac129ae81076cc5effde
riedlblower <github@michaelsexton.com>
2015-02-01 21:51:52 +0000

    Update 062_various_shorts_B.java

--- L062-Loops/062_various_shorts_B.java
+++ L062-Loops/062_various_shorts_B.java
@@ -27,3 +27,44 @@ public class CountingMatches
     }
 }
 //======================================
+// Bluej project: lesson6/counting_matches_digits
+// unit6.2-15
+/**
+ * A class that represents a number and can report
+ * properties of the number.
+ */
+public class Number
+{
+    private long number;
+
+    public Number(long number)
+    {
+        this.number = number;
+    }
+
+    /**
+     * Returns the number of times digitToMatch appears
+     * in this number.
+     * @param digitToMatch the digit to search for.
+     *   must be at least 0 and at most 9.
+     * @return number of occurrences of digitToMatch
+     */
+    public int countMatchingDigits(int digitToMatch)
+    {
+        // TODO: count and return the number of times the given digit appears in the number
+        // Hint: The last digit is (int)(numbers % 10).
+        int numberOfOccurances = 0;
+        long mainNumber = number;
+        while (mainNumber>0)
+        {
+            int remainder = (int)(mainNumber % 10);
+            mainNumber = mainNumber / 10;
+            if (remainder == digitToMatch)
+            {
+                numberOfOccurances++;
+            }
+        }
+        return numberOfOccurances;
+    }
+}
+//======================================
