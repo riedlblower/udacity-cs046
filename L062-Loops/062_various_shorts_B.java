d4f6cae616175fca12ae054c73e67debeb8ef1a2
riedlblower <github@michaelsexton.com>
2015-02-02 21:56:11 +0000

    Update 062_various_shorts_B.java

--- L062-Loops/062_various_shorts_B.java
+++ L062-Loops/062_various_shorts_B.java
@@ -147,3 +147,46 @@ public class FirstMatch
     }
 }
 //======================================
+// Bluej project: lesson6/position_of_first_match
+// Write a program that finds the first word in Alice In Wonderland
+// that is longer than 9 characters, and if there are no words
+// that long, prints "There are no long words".
+// unit6.2-19/20
+
+import java.util.Scanner;
+import java.io.FileReader;
+
+public class FirstMatch
+{
+    public static void main(String[] args) throws java.io.FileNotFoundException
+    {
+        Scanner in = new Scanner(new FileReader("aliceInWonderland.txt"));
+        String longWord = "";
+        boolean found = false;
+        int position = -1;
+
+        final int THRESHOLD = 9;
+
+        // TODO: Remember and print the position of the first word
+        // longer than THRESHOLD characters.
+        while (in.hasNext() && !found)
+        {
+            String word = in.next();
+            if (word.length() > THRESHOLD)
+            {
+                longWord = word;
+                found = true;
+            }
+            position++;
+        }
+        if (found)
+        {
+            System.out.println("The first long word is " + longWord + " at position " + position);
+        }
+        else
+        {
+            System.out.println("There are no long words");
+        }
+    }
+}
+//======================================
