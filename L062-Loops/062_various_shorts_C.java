51d5cc51c362e5439ae00de98c35a2055a860348
riedlblower <github@michaelsexton.com>
2015-02-16 22:25:38 +0000

    Update 062_various_shorts_C.java

--- L062-Loops/062_various_shorts_C.java
+++ L062-Loops/062_various_shorts_C.java
@@ -232,3 +232,40 @@ public class MonteCarlo
     }
 }
 //======================================
+// lesson6/throwing_a_die
+// unit6.2-27
+//
+// A program that simulates tossing a die 100 times. 
+// It prints the number of times you roll a 6. 
+import java.util.Random;
+
+public class ThrowingADie
+{
+    public static void main(String[] args)
+    {
+        // This line nitializes the generator and gives it 42 as a "seed." 
+        // The generator will generate a sequence of numbers that
+        // look random, but if you create another generator and seed 
+        // it with 42, it will generate the same sequence. 
+        Random generator = new Random(42);
+        int count = 0;
+        int rolled_dice;
+        // YOUR CODE HERE
+        // Use a loop to roll a die 100 times. 
+        // Count how many sixes you see, and print the number of
+        // 6's at the end. 
+        // Hint: if you're not sure how to simulate rolling a die, 
+        // check out the fact sheet on generating random numbers. 
+        for (int i = 1; i <= 100; i++)
+        {
+            rolled_dice = generator.nextInt(6) + 1;
+            if (rolled_dice == 6)
+            {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
+//======================================
+
