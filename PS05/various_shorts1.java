e0a1b12201d5b1b963ce028d5c0c0e0c4462c8db
riedlblower <github@michaelsexton.com>
2015-02-21 07:05:32 +0000

    Update various_shorts1.java

--- PS05/various_shorts1.java
+++ PS05/various_shorts1.java
@@ -15,3 +15,22 @@ public class StringTester
    }
 }
 //===============================================================
+//
+// Complete the method to return the product of
+// all the numbers 1 to the parameter n (inclusive)
+//
+
+public class MathUtil
+{
+   public int product(int n)
+   {
+       int result = 1;
+       for (int i = 1; i <=n; i++)
+       {
+           result = result * i;
+       }
+       return result;
+   }
+}
+//===============================================================
+
