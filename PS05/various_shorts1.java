5613c7051ec5237a1caae83a62766c8de14317f0
riedlblower <github@michaelsexton.com>
2015-02-21 07:15:52 +0000

    Update various_shorts1.java

--- PS05/various_shorts1.java
+++ PS05/various_shorts1.java
@@ -33,4 +33,32 @@ public class MathUtil
    }
 }
 //===============================================================
+//
+// Complete the method to return the number of non-vowels in the parameter
+// Assume the vowels are  a,e,i,o,u or A, E, I, O, U
+//
+
+public class Word
+{
+    /**
+     * Gets the number of non-vowels in the parameter
+     * @param word the string to count the non-vowels in
+     * @return the number of non-vowels in the word
+     */
+    public int nonVowelCount(String word)
+    {
+        int nonVowelCount = 0;
+        int length = word.length();
+        for (int i = 0; i < length; i++)
+        {
+            String letter = word.substring(i,i+1);
+            if (!"aeiouAEIOU".contains(letter))
+            {
+                nonVowelCount++;
+            }
+        }
+        return nonVowelCount;
+    }
+}
+//===============================================================
 
