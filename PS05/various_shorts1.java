d03f5d5536f67d8373f9b5ff93206d07efc47cbf
riedlblower <github@michaelsexton.com>
2015-02-21 07:43:18 +0000

    Update various_shorts1.java

--- PS05/various_shorts1.java
+++ PS05/various_shorts1.java
@@ -89,3 +89,35 @@ public class InputWithDo
     }
 }
 //===============================================================
+// hw05_11
+//
+// Complete this method to read integer scores from the user and find the average
+// Stop asking for input when the user enters a negative number.
+//
+// Keep a running total and keep track of the number of entries
+// then find and return the average
+//
+// Be sure not to divide by 0. Return 0 if no scores are entered
+//
+
+import java.util.Scanner;
+public class MathUtil
+{
+    public double averageScore()
+    {
+        Scanner in = new Scanner(System.in);
+        int score; // A user entered score
+        int runningTotal = 0;
+        int count = 0;
+        do
+        {
+            System.out.print("Enter a score. -1 to quit: ");
+            score = in.nextInt();
+            runningTotal = runningTotal + score;
+            count++;
+        }
+        while (score > -1);
+        return (runningTotal/count);
+    }
+}
+//===============================================================
