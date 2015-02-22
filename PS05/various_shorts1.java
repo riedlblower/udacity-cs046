33ff8a78c15e708e8f7b75f770d396134016cf9c
riedlblower <github@michaelsexton.com>
2015-02-22 09:08:06 +0000

    Update various_shorts1.java

--- PS05/various_shorts1.java
+++ PS05/various_shorts1.java
@@ -143,3 +143,35 @@ public class Drawings
         }
     }
 }
+
+//
+// This modifies the previous problem. Start with that solution as your base
+// Complete the code to write nested loops that make the a pattern of brackets: a
+// rows x colomuns figure of []
+// For example if rows = 2 and columns = 5, the figure will look like this
+//[][][][][]
+//[][][][][]
+//
+//
+
+//===============================================================
+//ps5-20
+public class Drawings
+{
+   /**
+    * Prints a rows by column array of blocks ([])
+    * @param rows the number of rows of []
+    * @param columns the number of columns of []
+    */
+    public void blockPrinter(int rows, int columns)
+    {
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                System.out.print("[]");
+            }
+            System.out.println("");
+        }
+    }
+}
