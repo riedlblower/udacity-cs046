7b2c6f692761d9cacbe3f9dfe4fca30a5b91386e
riedlblower <github@michaelsexton.com>
2015-02-22 09:07:33 +0000

    Update various_shorts1.java

--- PS05/various_shorts1.java
+++ PS05/various_shorts1.java
@@ -121,3 +121,25 @@ public class MathUtil
     }
 }
 //===============================================================
+//ps5-19
+// Complete the code to write nested loops that make the following pattern of brackets:
+//[][][][]
+//[][][][]
+//[][][][]
+//
+
+public class Drawings
+{
+
+    public void blockPrinter()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                System.out.print("[]");
+            }
+            System.out.println("");
+        }
+    }
+}
