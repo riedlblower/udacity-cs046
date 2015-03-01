ca99828c4eff488eab2f29fa575dd6475e46eddd
riedlblower <github@michaelsexton.com>
2015-03-01 09:06:57 +0000

    Update and rename L071-Arrays to L071-Arrays/05-GapBetweenPictures.java

---
+++ L071-Arrays/05-GapBetweenPictures.java
@@ -0,0 +1,32 @@
+// BlueJ project: lesson7/gallery2
+
+import java.util.ArrayList;
+
+public class ListOfPictures
+{
+    public static void main(String[] args)
+    {
+        ArrayList<Picture> gallery = new ArrayList<Picture>();
+        gallery.add(new Picture("degas1.jpg"));
+        gallery.add(new Picture("gaugin1.jpg"));
+        gallery.add(new Picture("monet1.jpg"));
+        gallery.add(new Picture("monet2.jpg"));
+        gallery.add(new Picture("renoir1.jpg"));
+
+        int width = 0;
+        for(int i = 1; i < gallery.size();i++)
+        {
+            Picture pic = gallery.get(i);
+            Picture left = gallery.get(i - 1);
+            width = left.getMaxX();
+            width = width + 10;
+            pic.translate(width, 0);
+        }
+        
+        for (int i = 0; i < gallery.size(); i++)
+        {
+            Picture pic = gallery.get(i);
+            pic.draw();
+        }
+    }
+}
