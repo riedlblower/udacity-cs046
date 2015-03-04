a1aec07a29594b22fa81416fac2975b7c706d057
riedlblower <github@michaelsexton.com>
2015-03-04 13:53:59 +0000

    Update and rename L071-Arrays to L071-Arrays/08-FriendsUpdated.java
    // This is part of the Person class from lesson 3.
    // A String is not the right way to store the friends of a Person
    // We need to refactor this code.
    // Change the friends variable to store an ArrayList<String>
    // that contains the names of the friends.
    // Re-implement the addFriend and getFriends methods.

---
+++ L071-Arrays/08-FriendsUpdated.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+public class Person
+{
+    private String name;
+    private ArrayList<String> friends;
+    private static final String SEPARATOR = ", ";
+
+    public Person(String name)
+    {
+        this.name = name;
+        this.friends = new ArrayList<String>();
+    }
+
+    /**
+     * Adds the given friend to this Person's friends list.
+     * @param friend the friend to add.
+     */
+    public void addFriend(Person friend)
+    {
+        friends.add(friend.name);
+    }
+
+    /**
+     * Gets a list of all of this Person's friends.
+     * @return the names of the friends separated by a comma and a space.
+     *     e.g. "Sara, Cheng-Han, Cay"
+     */
+    public String getFriends()
+    {
+//         return friends.toString();   <- not good as have to remove [] at ends.
+        String allFriends = friends.toString();
+        return allFriends.substring(1,allFriends.length()-1);
+    }
+
+    /**
+     * Gets a friend at a given index.
+     * @param friendIndex the index at which to find the friend (starting at 0)
+     * @return the name of the friend at friendIndex, or "" if the friend is not present
+     */
+    public String getFriend(int friendIndex)
+    {
+        String friendAtIndex = friends.get(friendIndex);
+        return friendAtIndex; 
+    }
+}
