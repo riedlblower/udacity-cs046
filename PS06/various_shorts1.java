f2895b954a8dd75d7d5a601aa093b7359f32f278
riedlblower <github@michaelsexton.com>
2015-03-22 08:15:16 +0000

    Update various_shorts1.java

--- PS06/various_shorts1.java
+++ PS06/various_shorts1.java
@@ -13,4 +13,40 @@ palette.add(Color.RED);
 
 Color color = get(8);
 
+==================================================
 4. 
+//
+// Complete the given class to display the picture object vertically
+// (rather than horizontally).
+// You can find the pictures required in the bluej project
+// Place the first picture so that the top of the picture is 10 pixels below 
+// the top of the canvas and put 10 pixels between the pictures
+
+import java.util.ArrayList;
+
+public class ListOfVerticalPictures
+{
+   public static void main(String[] args)
+   {
+      ArrayList<Picture> gallery = new ArrayList<Picture>();
+      gallery.add(new Picture("degas1.jpg"));
+      gallery.add(new Picture("gaugin1.jpg"));
+      gallery.add(new Picture("monet1.jpg"));
+      gallery.add(new Picture("monet2.jpg"));
+      gallery.add(new Picture("renoir1.jpg"));
+
+      // your code goes here
+      // TODO Write the code to display the pictures vertically
+        int height = 0;
+        for (Picture pic : gallery)
+        {      
+            height = height + 10;
+            pic.translate(0,height);
+            height = pic.getMaxY();
+            pic.draw();
+        }
+   }
+}
+
+==================================================
+5.
