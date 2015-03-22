91e8926514a2d534edf4750b613615f9bd507cb9
riedlblower <github@michaelsexton.com>
2015-03-22 08:03:47 +0000

    Create various_shorts1.java

---
+++ PS06/various_shorts1.java
@@ -0,0 +1,16 @@
+1. declare and instantiate and ArrayList of Colors objects using the Color class;
+
+ArrayList<Color> palette = new ArrayList<Color>();
+
+left hand side: declares that palette is a reference to an ArrayList object. 
+right hand side: creates (instantiates) an ArrayList object and returns a reference. 
+
+2. add Color.Red to the ArrayList
+
+palette.add(Color.RED);
+
+3. get last element in ArrayList of 9 colors;
+
+Color color = get(8);
+
+4. 
