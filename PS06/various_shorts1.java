cc1bc8f1a6265ce315194dacaed72ffae1a47395
riedlblower <github@michaelsexton.com>
2015-03-24 06:42:54 +0000

    Update various_shorts1.java

--- PS06/various_shorts1.java
+++ PS06/various_shorts1.java
@@ -57,4 +57,38 @@ int size = palette.size()
 
 Color color = palette.get(palette.size()-1);
 
+==================================================
 7.
+//
+// Complete the given class to shift the first picture (the one at index 0)
+// to the end of the array list and  display the pictures object in the modified list
+// horizontally as was done in the lesson.  Put 10 pixels between pictures.
+// You can find the pictures required in in the bluej project
+//
+
+import java.util.ArrayList;
+
+public class ShiftFirstToEnd
+{
+   public static void main(String[] args)
+   {
+      ArrayList<Picture> gallery = new ArrayList<Picture>();
+      gallery.add(new Picture("degas1.jpg"));
+      gallery.add(new Picture("gaugin1.jpg"));
+      gallery.add(new Picture("monet1.jpg"));
+      gallery.add(new Picture("monet2.jpg"));
+      gallery.add(new Picture("renoir1.jpg"));
+
+        gallery.add(gallery.get(0));
+        gallery.remove(0);
+        int width = 0;
+        for (Picture pic : gallery)
+        {      
+            width = width + 10;
+            pic.translate(width,0);
+            width = pic.getMaxX();
+            pic.draw();
+        }
+   }
+}
+==================================================
