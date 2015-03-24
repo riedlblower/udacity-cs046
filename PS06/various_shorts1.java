04c147128d1060de30eb44a3d8ac9fdef7b8eb2a
riedlblower <github@michaelsexton.com>
2015-03-24 07:03:06 +0000

    Update various_shorts1.java

--- PS06/various_shorts1.java
+++ PS06/various_shorts1.java
@@ -129,3 +129,32 @@ public class AddTester
 }
 ==================================================
 11.
+// Complete this class. An array list of Strings called cities has been defined
+// for you and partially populated. Add code to do the following:
+// 1. Add the string "New York so that it is first in the arraylist
+// 2. replace the string "Dallas" (should be at index 2. Don't search) with "Houston"
+// 3. remove last element in the list
+// 4. print the arraylist
+
+import java.util.ArrayList;
+public class ArrayListMethodTester
+{
+   public static void main(String[] args)
+   {
+       ArrayList<String> cities = new ArrayList<String>();
+       cities.add("Chicago");
+       cities.add("Dallas");
+       cities.add("San Jose");
+       cities.add("Seattle");
+
+       // TODO: Add code for steps 1-4 above
+       cities.add(0,"New York");
+       cities.remove("Dallas");
+       cities.add(2,"Houston");
+       cities.remove(cities.get(cities.size()-1));
+       // TODO: Print the arraylist
+       System.out.println(cities);
+       System.out.println("Expected: [New York, Chicago, Houston, San Jose]");
+   }
+}
+==================================================
