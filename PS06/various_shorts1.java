8434dc027606d595f453c369c94859c757c28289
riedlblower <github@michaelsexton.com>
2015-03-27 06:55:59 +0000

    Update various_shorts1.java

--- PS06/various_shorts1.java
+++ PS06/various_shorts1.java
@@ -216,3 +216,29 @@ public class StringArrays
     }
 }
 ==================================================
+15.
+// Complete the method in this class to compute and return the shortest string
+// (the one with the fewest characters)
+// HINT: You can use the ArrayList algorithm for finding the minimum
+import java.util.ArrayList;
+public class StringArrayUtil
+{
+   public String shortestString(ArrayList<String> words)
+   {
+      // TODO: initialize the string to the hold the shortest string
+      String shortest = words.get(0); //initialize
+      int shortestLength = shortest.length();
+
+      // TODO: Write the code to find the shortest string
+       for (String word : words)
+       {
+           int length = word.length();
+           if (length < shortestLength)
+           {
+//               shortest = word;
+                shortest.equals(word);
+           }
+       }
+      return shortest;
+   }
+}
