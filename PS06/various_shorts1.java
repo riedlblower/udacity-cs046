3e1d5ec1adf55dcb60158276372dce11a128594b
riedlblower <github@michaelsexton.com>
2015-03-28 08:43:00 +0000

    Update various_shorts1.java

--- PS06/various_shorts1.java
+++ PS06/various_shorts1.java
@@ -350,3 +350,35 @@ public class Finder
     }
 }
 ==================================================
+20.
+ * Complete the method in this class to return the first string
+ * that starts with the target. Return null if no string starts with the target
+
+import java.util.ArrayList;
+public class StringFinder
+{
+   /**
+    * Gets the first String that starts with the target
+    * @param text the array list to process
+    * @param target the desired letter
+    * @return the first string starting with target or null if not found
+    */
+    public String getTarget(ArrayList<String> text, String target)
+    {
+        int i = 0;
+        String startsWith = null;
+        boolean found = false;
+        while (i < text.size() && found == false) //supply condition
+        {
+           //your code goes here
+            if (text.get(i).startsWith(target))
+                {
+                    startsWith = text.get(i);
+                    found = true;
+                }
+            i++;
+            }
+        return startsWith;
+    }
+}
+==================================================
