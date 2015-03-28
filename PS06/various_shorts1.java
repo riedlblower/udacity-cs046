d454f24d631ce2a7b626796816b21eb346c624a0
riedlblower <github@michaelsexton.com>
2015-03-28 08:09:19 +0000

    Update various_shorts1.java

--- PS06/various_shorts1.java
+++ PS06/various_shorts1.java
@@ -312,3 +312,41 @@ public class Counter
         }
 }
 ==================================================
+19.
+// Complete the method in this class to return the index of the first string
+// that starts with the target. Return -1 if no string starts with the target
+
+import java.util.ArrayList;
+public class Finder
+{
+   /**
+    * Gets the index of the first String that starts with the target
+    * @param text the array list to process
+    * @param target the desired letter
+    * @return the index of first string starting with target or -1 if not found
+    */
+    public int getIndex(ArrayList<String> text, String target)
+    {
+        int i = 0;
+        int index = -1;
+        boolean found = false;
+        int length = text.size();
+        while (length > 0) //supply condition
+        {
+            // TODO: Adapt an ArrayList algorithm to return the index of the first string that start with the target
+            // TODO: Return -1 if no string starts with the target
+            for (String word: text)
+            {
+                if (word.startsWith(target) && found == false)
+                {
+                    index = i;
+                    found = true;
+                }
+                i++;
+                length--;
+            }
+        }
+        return index;
+    }
+}
+==================================================
