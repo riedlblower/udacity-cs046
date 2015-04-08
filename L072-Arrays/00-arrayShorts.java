467b861103649409e476b49f7143ccbabdff7a0d
riedlblower <github@michaelsexton.com>
2015-04-08 06:59:13 +0100

    Create 00-arrayShorts.java

---
+++ L072-Arrays/00-arrayShorts.java
@@ -0,0 +1,14 @@
+
+3. Declare an array of five primes
+int primes [] = {2,3,5,7,11};
+
+5. Setting first and last values to 10 for a 10 element array
+values[0] = 10;
+values[values.length-1] = 10;
+
+6. Declaring array of strings
+String[] array = new String[10];
+String[] array = {"yes", "no"};
+
+
+
