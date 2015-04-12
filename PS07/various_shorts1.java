ccb95658066ee160bf7fbedc2c7b9ec32370d885
riedlblower <github@michaelsexton.com>
2015-04-12 22:21:54 +0100

    Update various_shorts1.java

--- PS07/various_shorts1.java
+++ PS07/various_shorts1.java
@@ -6,3 +6,46 @@ double[] prices;
 
 int[] scores = new int[50];
 
+3. double[] temperatures = new double[12];
+=> this array can hold 12 elements and 12 is the length of the array.
+
+4. double[] temperatures = new double[12];
+=> initial value at index 5 is 0.0
+
+5. double[] temperatures = new double[12];
+print out the element at index 9.
+
+System.out.println(temperatures[9]);
+
+6. Write the statement to assign 95 to the first element of an int array scores
+
+scores[0] = 95;
+
+7. Complete the code to initialise an array containing the prime numbers less than 10.
+
+int[] primes = {2,3,5,7};
+
+8. double[] temperatures = new double[12];
+=> the indeces range from 0 to 11.
+
+9. You have an int array variable scores  of unknown length.
+Complete the code to get the length of the array. You can assume the scores is not null.
+
+int length = scores.length;
+
+
+ 
+ 
+ 
+
+
+
+
+
+
+
+
+
+
+
+
