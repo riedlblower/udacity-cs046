6da898805b74b9ebefa4bb0b5a8379b96a054575
riedlblower <github@michaelsexton.com>
2015-04-16 22:12:28 +0100

    Update various_shorts1.java

--- PS07/various_shorts1.java
+++ PS07/various_shorts1.java
@@ -39,6 +39,28 @@ scores[scores.length-1] = 98;
 11. Print the first element of an array "scores" of unknown length.
 System.out.println(scores[0]);
  
+16.
+// Complete the method in this class. It returns a string with the elements of the array
+// in reverse order separated by a space. If the array looks like this [1, 2 ,3 ,4], your
+// output will look like this 4 3 2 1
+public class ArrayUtil
+{
+  /**
+   * Returns a string with the elements of the array in reverse order separated by a space
+   * @param values the array to reverse
+   * @return the string containing the elements in reverse order
+   */
+  public String inReverse(int[] values)
+  {
+      String message ="";
+      for ( int value : values) //finish the loop header
+      {
+          // TODO: concatenate the elements to message in reverse order
+          message = String.valueOf(value) + " " + message;
+      }
+      return message;
+  }
+}
 
 
 
