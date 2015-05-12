8ba4109eb92b925917338ab30be070d7c119355b
riedlblower <github@michaelsexton.com>
2015-05-12 07:06:08 +0100

    Create 07-initialiseArrayListAndMethods.java

---
+++ L08-Classes/07-initialiseArrayListAndMethods.java
@@ -0,0 +1,55 @@
+// BlueJ project: lesson8/collecting
+
+// TODO: Initialize the array list
+// TODO: Implement the addPassenger and getPassengerCount methods
+
+import java.util.ArrayList;
+
+public class Car
+{
+    private int numberOfSeats;
+    private ArrayList<String> passengers;    //declare ArrayList
+    
+
+    /**
+        Constructs a car with a given number of seats
+        @param numberOfSeats the number of seats in this car.
+    */
+    public Car(int numberOfSeats)
+    {
+        this.numberOfSeats = numberOfSeats;
+        passengers = new ArrayList<String>();     //initialise ArrayList
+    }
+
+    /**
+       Adds a passenger to this car, provided that there is space.
+       @param name the name of the passenger
+    */
+    public void addPassenger(String name)
+    {
+        if (getPassengerCount() < numberOfSeats -1) 
+            //see if seat available and driver takes up a seat too, hence the -1
+        {
+            passengers.add(name);
+        }
+
+    }
+
+    /**
+       Gets the number of passengers in this car (not counting the driver).
+       @return the number of passengers
+    */
+    public int getPassengerCount()
+    {
+        return passengers.size();
+    }
+
+    /**
+       Gets a list of all passengers in this car.
+       @return a list of the form [name1, name2, ...]
+    */
+    public String getPassengerList()
+    {
+        return passengers.toString();
+    }
+}
