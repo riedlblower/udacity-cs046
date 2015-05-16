984bfe2b5b8b178a419b42d9ba5955e2e7a6c0a0
riedlblower <github@michaelsexton.com>
2015-05-16 22:25:53 +0100

    Create 14-staticVariableForId.java

---
+++ L08-Classes/14-staticVariableForId.java
@@ -0,0 +1,46 @@
+// BlueJ project: lesson8/friends
+
+import java.util.ArrayList;
+
+public class Person
+{
+    private static int lastAssignedId = 0;
+    
+    private String name;
+    private int id;
+    private ArrayList<Person> friends;
+
+    // TODO: Assign IDs 1, 2, 3, ... to each Person object
+    // Use a static variable to track the last ID that was assigned.
+    
+    public Person(String name)
+    {
+        this.name = name;
+        friends = new ArrayList<Person>();
+        lastAssignedId++;
+        id = lastAssignedId; 
+    }
+
+    /**
+     * Adds the given friend to this Person's friends list.
+     * @param friend the friend to add.
+     */
+    public void addFriend(Person friend)
+    {
+        friends.add(friend);
+    }
+    
+    public String toString()
+    {
+        return "Person[id=" + id + ",name=" + name + "]";
+    }
+ 
+    /**
+     * Gets the number of friends this Person has.
+     * @return the number of friends.
+     */
+    public int getNumFriends()
+    {
+        return friends.size();
+    }
+}
