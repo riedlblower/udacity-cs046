ea642db3308c3c8925703a88e9565aceb3470d7f
riedlblower <github@michaelsexton.com>
2015-05-20 06:44:46 +0100

    Create 17-carDrive&Drop.java

---
+++ L08-Classes/17-carDrive&Drop.java
@@ -0,0 +1,83 @@
+// BlueJ project: lesson8/carshare2
+
+    
+import java.util.ArrayList;
+
+public class Car
+{
+    private ArrayList<Person> passengers;
+    private int numberOfSeats;
+    private String driverName;
+    private int location;
+    private int destination;
+
+    public Car(String driverName, int numberOfSeats, int destination)
+    {
+        passengers = new ArrayList<Person>();
+        this.driverName = driverName;
+        this.numberOfSeats = numberOfSeats;
+        location = 0;
+        this.destination = destination;
+    }
+
+    /**
+       Gets the name of this car's driver.
+       @return the driver name
+    */
+    public String getDriverName()
+    {
+        return driverName;
+    }
+
+    /**
+       Checks whether this car has arrived at its destination.
+       @return true if the car has arrived
+    */
+    public boolean hasArrived()
+    {
+        return location == destination;
+    }
+
+    /**
+       Tries to add a passenger.
+       @param p the new passenger
+       @return true if there is room in the car and the passenger's
+       destination on the way to this car's destination
+    */
+    public boolean tryToAdd(Person p)
+    {
+        if (passengers.size() + 2 <= numberOfSeats
+                && p.getDestination() <= destination)
+        {
+            passengers.add(p);
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    /**
+       Drives this car to the next location and drops off
+       any passengers whose destination is that location
+    */
+    public void drive()
+    {
+        // TODO: Implement this method
+        location = location + 1;     //drive to relevant location
+        for (int i = 0; i <= passengers.size()-1; i++)
+        {
+            if (passengers.get(i).getDestination() == location)
+            {
+                passengers.remove(i);
+            }
+        }
+        return;
+    }
+ 
+    public String toString()
+    {
+        return "Car[location=" + location + ",driverName=" + driverName + ",passengers=" + passengers + "]";
+    }   
+}
