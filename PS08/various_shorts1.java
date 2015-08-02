aca903c68f7417b587686249080c6c6db538ad24
riedlblower <github@michaelsexton.com>
2015-08-02 21:48:33 +0100

    Update various_shorts1.java

--- PS08/various_shorts1.java
+++ PS08/various_shorts1.java
@@ -1,4 +1,4 @@
-1. declare and instantiate an array of ints (4 row, 5 columns).
+1. Declare and instantiate a two dimensional array of ints with 4 rows and 5 columns.
 
 int [][] numbers = new int[4][5];
 
