96ffb48ab7a6b3e9000609290f0925b33d2cce9d
riedlblower <github@michaelsexton.com>
2015-08-03 07:40:32 +0100

    Update various_shorts1.java

--- PS08/various_shorts1.java
+++ PS08/various_shorts1.java
@@ -21,3 +21,39 @@ matrix[matrix.length-1][matrix[0].length-1] = 54;
 6. Print out the last entry of the last row of the matrix.
 
 System.out.println(matrix[matrix.length-1][matrix[0][length-1]);
+
+========================================================
+7.// Complete this method to print a 2D array of ints in column and row format.
+// Use printf with a format specifier of "%6d"
+//     System.out.printf("%6d", ...);
+// We will assume that none of the arrays the method is asked to print will
+//have values greater than 999. You will need to use nested loops.
+//
+// Notice that the method is static. That is because it does not work on the
+//instance variables of the class. It gets all its data as parameters
+
+public class ArrayUtil
+{
+    /**
+     * Prints the given 2D array in columns and rows
+     * @param the array to print
+     */
+    public static void print2D(int[][] matrix)
+    {
+        // TODO: Complete this method to print a 2D array of ints in column and row format.
+        int rows = matrix.length;
+        int columns = matrix[0].length;
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                System.out.printf("%6d", matrix[i][j]);
+            }
+            System.out.println();
+            
+        }
+    }
+
+}
+
+========================================================
