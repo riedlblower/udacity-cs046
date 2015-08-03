cf8f71a8121a3bdde55e934ab8c6430080624d2b
riedlblower <github@michaelsexton.com>
2015-08-03 07:57:03 +0100

    Update various_shorts1.java

--- PS08/various_shorts1.java
+++ PS08/various_shorts1.java
@@ -57,3 +57,29 @@ public class ArrayUtil
 }
 
 ========================================================
+8.
+// Complete the method to fill a 2D array of Strings with all X's ("X")
+// Use nested loops
+//
+// Note that the method is static. It does not use the instance variables of
+//the class. It gets all the information it needs as a parameter
+
+public class ArrayUtil
+{
+     /**
+     * Fills the given 2D array with X's ("X").
+     * @param the array to fill
+     */
+    public static void fill2D(String[][] matrix)
+    {
+        // TODO:  Complete the method to fill a 2D array of Strings with all X's ("X")
+        for (int i = 0; i < matrix.length; i++)
+        {
+            for (int j = 0; j < matrix[0].length; j++)
+            {
+                matrix[i][j] = String.valueOf('X');
+            }
+        }
+    }
+}
+========================================================
