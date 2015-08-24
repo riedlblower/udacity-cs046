93693423e2811411ebf3bea26b69d0d22b0cb04b
riedlblower <github@michaelsexton.com>
2015-08-24 07:37:51 +0100

    Create 08-ball.java

---
+++ L09-InterfacesInheritance/08-ball.java
@@ -0,0 +1,26 @@
+// BlueJ project: lesson9/scene3
+// TODO: Make this ball drawable.
+// To draw it, simply fill the shape.
+
+public class Ball implements Drawable
+{
+    private Ellipse shape;
+
+    /**
+       Constructs a ball.
+       @param xLeft the leftmost x-coordinate of this ball.
+       @param yTop the topmost y-coordinate of this ball.
+       @param color the color of this ball.
+    */
+    public Ball(int xLeft, int yTop, Color color)
+    {
+        final int DIAMETER = 40;
+        shape = new Ellipse(xLeft, yTop, DIAMETER, DIAMETER);
+        shape.setColor(color);
+    }
+    
+    public void draw()
+    {
+        shape.fill();
+    }
+}
