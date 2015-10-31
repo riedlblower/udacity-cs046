48e301c455305043d64a393e3ef12979713db806
riedlblower <github@michaelsexton.com>
2015-10-31 13:15:39 +0000

    Create various_shorts1.java

---
+++ PS09/various_shorts1.java
@@ -0,0 +1,23 @@
+1. We want to define an interface Growable. Many things are Growable. Fill in the blank in the given header.
+
+public interface Growable 
+{ ... }
+
+2. We want to declare an interface Readable. It has one method read(). Read() takes no parameters and is void. Complete the code
+
+public interface Readable
+{  void read();  }
+
+3. A class Dog wishes to implement the Growable interace. Complete the class header to do that.
+
+public class Dog implements Growable
+
+4. The class Billboard implements the Readable interface. It has one method read() which is void and takes no parameters. Bill board may contain many methods but there is one method that is required. Provide the header for that method
+
+public void read()
+{  ..  }
+
+5. What is the name of the metod that a class must provide if the class implements the Comparable interface from the standard Java library?
+
+compareTo()
+
