28e319877c2260ab79ebd8e3f7a526a12acab609
riedlblower <github@michaelsexton.com>
2015-11-02 06:32:26 +0000

    Update various_shorts1.java

--- PS09/various_shorts1.java
+++ PS09/various_shorts1.java
@@ -21,3 +21,43 @@ public void read()
 
 compareTo()
 
+6. // Here is a class called UnitCircle. Its radius is an int. A couple of methods have been
+// provided.
+
+// Add the code so that the UnitCircle class implements the Comparable interface
+
+/**
+ * Represents a circle whose radius is multiples of the unit circle - a circle with radius 1
+ */
+// TODO: add everything necessary for this class to implement Comparable
+// HINT: There are two things you need to do
+public class UnitCircle implements Comparable<UnitCircle>
+{
+    private int radius;
+
+    public UnitCircle(int radius)
+    {
+        this.radius = radius;
+    }
+
+    public int getRadius()
+    {
+        return radius;
+    }
+
+    public String toString()
+    {
+        return "UnitCircle[r=" + radius + "]";
+    }
+
+    //... other methods
+    public int compareTo(UnitCircle x)
+    {
+        int answer = x.getRadius();
+        if (answer < radius) {return 1;}
+        else if (answer == radius) {return 0;}
+        else return -1;
+    }
+}
+
+=============================================================================
