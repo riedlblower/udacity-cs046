c372298b5a344eb91beb0296be297e2825771695
riedlblower <github@michaelsexton.com>
2015-11-12 20:48:35 +0000

    Update various_shorts1.java

--- PS09/various_shorts1.java
+++ PS09/various_shorts1.java
@@ -184,4 +184,34 @@ Answer: Mammal
 public class Motorcycle extends Vehicle
 
 =============================================================================
+17. 
+// This is the first of two problems to use these classes
+// The manager class is a subclass of Employee. A Manager is different from an employee in that
+// a Manager has a bonus. So we add that instance variable
 
+// Add the code to complete the constructor
+
+public class Manager extends Employee
+{
+    private double bonus;
+
+    /**
+     * Constructs a Manager
+     * @name the name of the manager
+     * @salary the salary of this manager
+     * @param theBonus the bonus for this Manager
+     */
+    public Manager(String name, double salary, double theBonus)
+    {
+        // TODO: complete the constructor
+        super(name,salary);
+        this.bonus = theBonus;
+    }
+
+    public double getBonus()
+    {
+        return bonus;
+    }
+
+}
+=============================================================================
