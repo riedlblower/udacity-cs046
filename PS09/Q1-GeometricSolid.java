9adc36bd37ccbc6cd28cd8683bcd72e242bfc99d
riedlblower <github@michaelsexton.com>
2015-11-14 21:51:56 +0000

    Create Q1-GeometricSolid.java

---
+++ PS09/Q1-GeometricSolid.java
@@ -0,0 +1,16 @@
+// This problem has several parts.
+// First complete the GeometricSolid interface. The interface has two methods
+//      1) getVolume 
+//      2) getSurfaceArea
+// both of which return a double
+//
+// Then modify the both the Shere and the Cube method to implement the
+// GeometricSolid interface
+//
+
+public interface GeometricSolid
+{
+    double getVolume();
+    double getSurfaceArea();
+
+}
