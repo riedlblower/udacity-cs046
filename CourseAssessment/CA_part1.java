6b5d8ed78df632240ca273ddcc5a01441cd3433b
riedlblower <github@michaelsexton.com>
2015-12-28 17:46:00 +0000

    Update CA_part1.java

--- CourseAssessment/CA_part1.java
+++ CourseAssessment/CA_part1.java
@@ -11,4 +11,19 @@ String upper = name.toUpperCase();
 
 4. You need to declare your instance variables as private so that they can only be accessed by methods of the same class. And not by methods of other classes.
 
-5. 
+
+5. How do add in description of a method when returning a variable
+/** Gets the perimeter of this Circle object
+* @return the perimeter
+*/
+
+6. answer = none of the above
+
+7. Write the code to declare volume as a local variable of type double and then initialize it to 0
+double volume = 0;
+
+8. Write the code to declare an integer constant HOURS_PER_DAY in a METHOD. Initialise it to 24
+final int HOURS_PER_DAY = 24;
+
+9. Complete the if statement to test if an int variable score is exactly equal to 90.
+if (score == 90)
