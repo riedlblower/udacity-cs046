b34bf21c916b02e460170122f5207ee20649ea6b
riedlblower <github@michaelsexton.com>
2015-12-28 17:34:02 +0000

    Create CA_part1.java

---
+++ CourseAssessment/CA_part1.java
@@ -0,0 +1,14 @@
+Course Assessment Part 1 (Intro to Java Programming)
+
+1. Complete the line of code to fine the length of a String name
+int length = name.length();
+
+2. Complete the code to get ta substring of the String name which contains just the first letter
+String first = name.substring(0,1);
+
+3. Complete the code to get an uppercase version of the String name
+String upper = name.toUpperCase();
+
+4. You need to declare your instance variables as private so that they can only be accessed by methods of the same class. And not by methods of other classes.
+
+5. 
