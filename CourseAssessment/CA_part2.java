867e076345f96a9cc757b322b5b9cb673a5d2a88
riedlblower <github@michaelsexton.com>
2015-12-31 12:56:51 +0000

    Update CA_part2.java

--- CourseAssessment/CA_part2.java
+++ CourseAssessment/CA_part2.java
@@ -281,3 +281,59 @@ public class RectangleList
     }
 }
 =============================
+22. Flag Drawing
+
+// Here is the flag of Gabon. It has three equal horizontal lines of green, yellow,
+// and blue. You are provided with the familiar Flag class. You are to complete the getColorAt
+// method to return the correct color of each x,y so that the flag is drawn correctly
+//
+// See the image at this URL:
+// http://i.imgur.com/FMaRlE8.png
+//   
+// You must use the else if construction
+
+public class Flag
+{
+    private Picture pic;
+    private int width;
+    private int height;
+
+    public Flag(int width, int height)
+    {
+        this.width = width;
+        this.height = height;
+        pic = new Picture(width, height);
+        pic.draw();
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                Color c = getColorAt(x, y);
+                pic.setColorAt(x, y, c);
+            }
+        }
+    }
+
+    public Color getColorAt(int x, int y)
+    {
+        // TODO: add correct conditions to determine which color needs to be used at any
+        // (x, y) coordinate in order to produce the Gabon flag
+        // TODO: set the variable c to that color
+        // TODO: use else if
+        Color c;
+        if ((x < width) && (y>=2*height/3 && y<height))
+        {
+            c = Color.BLUE;
+        }
+        else if ((x < width) && (y>=height/3 && y<2*height/3))
+        {
+            c = Color.YELLOW;
+        }
+        else
+        {
+            c = Color.GREEN;
+        }
+        return c;
+    }
+}
+=============================
