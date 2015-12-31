90ef4bf0f643b61393a377b45b5dd710f52ae458
riedlblower <github@michaelsexton.com>
2015-12-31 17:16:05 +0000

    Update CA_part2.java

--- CourseAssessment/CA_part2.java
+++ CourseAssessment/CA_part2.java
@@ -337,3 +337,42 @@ public class Flag
     }
 }
 =============================
+23. Number Pyramid
+
+// Write code in this main method to print a pyramid of numbers like this:
+//
+// 1
+// 2   2
+// 3   3   3
+// 4   4   4   4
+// 5   5   5   5   5
+// 6   6   6   6   6   6
+// 7   7   7   7   7   7   7
+// 8   8   8   8   8   8   8   8
+// 9   9   9   9   9   9   9   9   9
+//10  10  10  10  10  10  10  10  10  10
+//
+// You must use nested loops. Do not print each row individually.
+// To format your table like the model, use a printf statement like this:
+// System.out.printf("%4d", ... );
+
+public class NumberPyramid
+{
+   /**
+    * prints rows of numbers
+    * @param rows the number of rows to print
+    */
+    public void print(int rows)
+    {
+        // TODO: implement the method to print the specified number of rows
+        for (int i = 1; i <= rows; i++)
+        {
+            for (int j = 1; j<=i; j++)
+            {
+                System.out.printf("%4d",i);
+            }
+        System.out.println("");
+        }
+    }
+}
+=============================
