c0fc0ca69bf4acd9fe0a0a58470fac12d5a9172b
riedlblower <github@michaelsexton.com>
2016-01-02 21:38:18 +0000

    Create CA_part8_Q31.java

---
+++ CourseAssessment/CA_part8_Q31.java
@@ -0,0 +1,23 @@
+Course Assessment Part 8 (Intro to Java Programming)
+
+===============================
+
+31. Loops
+
+Question: Come up with a situation in which you would want to use a for loops.
+Why would a while loop not work as well for this case?
+
+Answer: I would use a for loop when I know how many loops to iterate
+e.g. for i = 0; i < 24; i++
+e.g. one for each hour of the day
+
+Question: Now describe a scenario in which a while loop would work better
+than a for loop. What makes a while loop more suitable this time?
+
+Answer: I use a while loop when the number of loops in unknown beforehand
+e.g. while (electricty_on == false)
+e.g. while the switch is off.
+
+--------------------------------------------------
+
+32. Interfaces or Inheritance?
